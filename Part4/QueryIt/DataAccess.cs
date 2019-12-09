d57662ce9ca99063b7eb4bc0c36a21b837b04477
Zoltan Both <zoltan.both@sap.com>
2019-12-09 09:14:30 +0100

    Part 4 - Started

---
+++ Part4/QueryIt/DataAccess.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryIt
+{
+    public class EmployeeDb : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=QueryIt.db");
+    }
+
+    public interface IRepository<T> : IDisposable
+    {
+        void Add(T newEntity);
+        void Delete(T entity);
+        T FindById(int Id);
+        IQueryable<T> FindAll();
+        int Commit();
+    }
+
+    public class EmployeeRepository<T> : IRepository<T> where T : class, IEntity
+    {
+        private readonly DbContext _ctx;
+        DbSet<T> _set;
+
+        public EmployeeRepository(DbContext ctx)
+        {
+            _ctx = ctx;
+            _set = ctx.Set<T>();
+        }
+
+        public void Add(T newEntity)
+        {
+            if (newEntity.IsValid())
+            {
+                _set.Add(newEntity);
+            }
+        }
+
+        public int Commit()
+        {
+            return _ctx.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return _set;
+        }
+
+        public T FindById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Part4/QueryIt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace QueryIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IRepository<Employee> employeeRepository = new EmployeeRepository<Employee>(new EmployeeDb()))
+            {
+                //AddEmployees(employeeRepository);
+                CountEmployees(employeeRepository);
+            }
+        }
+
+        private static void AddEmployees(IRepository<Employee> employeeRepository)
+        {
+            // employeeRepository.Add(new Employee { Name = "Scott" });
+            employeeRepository.Add(new Employee { Name = "Alex" });
+            employeeRepository.Commit();
+        }
+
+        private static void CountEmployees(IRepository<Employee> employeeRepository)
+        {
+            Console.WriteLine(employeeRepository.FindAll().Count());
+        }
+    }
+}
